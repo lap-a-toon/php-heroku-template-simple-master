69093935692086b9211aef72845557c4c98b6540
Xander Lexx <lap-a-toon@yandex.ru>
2024-03-19 13:25:08 +0500

    переделал

--- web/index.php
+++ web/index.php
@@ -1,3 +1,33 @@
 <?php
+session_start();
+$author_data = file_exists('author.php') ? include_once('author.php') : "";
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?=$author_data;?>
+<?php 
+if(!isset($_SESSION['username'])){
+    ?>
+    <form action="post.php" method="POST">
+        <label for="name">Представьтесь</label>
+        <input type="text" name="name" id="name">
+        <input type="submit" value="Зайти">
+    </form>
+    <?php
+}else{
+    ?>
+    <h3>Приветствую Вас, <?=$_SESSION['username']?>!</h3>
+    <a href="exit.php">Выход</a>
+    <?php
+}
+?>
+</body>
+</html>
+<?php
 
-echo 'I\'m alive!';
